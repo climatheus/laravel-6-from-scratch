b20f94416c4c35febf17631accc8dee508105b95
Matheus <santosaamatheus@gmail.com>
2021-03-09 23:22:51 -0300

    First commit

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+        // render a list of a resource
+
+        $articles = Article::latest()->get();
+
+        return view('articles.index', [
+            'articles' => $articles
+        ]);
+    }
+
+    public function show($id)
+    {
+        // show a single resource
+
+        $article = Article::find($id);
+
+        return view('articles.show', [
+            'article' => $article
+        ]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required',
+        ]);
+
+        $article = new Article();
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+        $article->save();
+
+        return redirect('/articles');
+    }
+
+    public function edit($id)
+    {
+        $article = Article::find($id);
+
+        return view('articles.edit', [
+            'article' => $article
+        ]);
+    }
+
+    public function update($id)
+    {
+        request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required',
+        ]);
+
+        $article = Article::find($id);
+
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+        $article->save();
+
+        return redirect('/articles/' . $article->id);
+    }
+
+    public function destroy()
+    {
+        // delete a resource
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'articles' => App\Article::take(3)->latest()->get()
+    ]);
+});
+
+Route::get('/articles', 'ArticlesController@index');
+Route::post('/articles', 'ArticlesController@store');
+Route::get('/articles/create', 'ArticlesController@create');
+Route::get('/articles/{id}', 'ArticlesController@show');
+Route::get('/articles/{id}/edit', 'ArticlesController@edit');
+Route::put('/articles/{id}', 'ArticlesController@update');
